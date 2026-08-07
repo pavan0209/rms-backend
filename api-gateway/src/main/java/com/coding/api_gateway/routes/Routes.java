be43aaa5a6c6736ae7378832d0b993413dcbb0fe
pavan0209 <pavanasonawane@gmail.com>
2026-08-07 15:25:31 +0530

    refactor: : temporarily disable Eureka Server and added routes

---
+++ api-gateway/src/main/java/com/coding/api_gateway/routes/Routes.java
@@ -0,0 +1,43 @@
+package com.coding.api_gateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+    @Bean
+    RouterFunction<ServerResponse> productServiceRoute() {
+        return GatewayRouterFunctions.route("productMS")
+                .route(
+                        RequestPredicates.path("/api/product/**"),
+                        HandlerFunctions.http("http://localhost:8080")
+                )
+                .build();
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> orderServiceRoute() {
+        return GatewayRouterFunctions.route("orderMS")
+                .route(
+                        RequestPredicates.path("/api/order/**"),
+                        HandlerFunctions.http("http://localhost:8081")
+                )
+                .build();
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return GatewayRouterFunctions.route("inventoryMS")
+                .route(
+                        RequestPredicates.path("/api/inventory/**"),
+                        HandlerFunctions.http("http://localhost:8082")
+                )
+                .build();
+    }
+}
